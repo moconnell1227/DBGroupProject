8fe6a2e05c10df233d7efb7559b2bf38e3c07d5f
nabishop <35745821+nabishop@users.noreply.github.com>
2019-12-02 08:13:15 -0800

    changed structure

--- src/Daos/DaoManager.java
+++ src/Daos/DaoManager.java
@@ -1,3 +1,5 @@
+package Daos;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
--- src/Models/Dao.java
+++
@@ -1,9 +0,0 @@
-import java.util.Set;
-
-public interface Dao<T> {
-  T getById(int id);
-  Set<T> getAll();
-  Boolean insert(T obj);
-  Boolean update(T obj);
-  Boolean delete(T obj);
-}
