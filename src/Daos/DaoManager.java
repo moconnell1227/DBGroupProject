3f7421f48e923f4d6364caae3be8a7bc89b3271b
nabishop <35745821+nabishop@users.noreply.github.com>
2019-12-02 08:21:17 -0800

    Update DaoManager.java

--- src/Daos/DaoManager.java
+++ src/Daos/DaoManager.java
@@ -1,85 +1,90 @@
 package Daos;
 
+import Models.ConnectionFactory;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class DaoManager{
-  protected ConnectionFactory connFac = null;
-  protected Connection conn = null;
 
-  public DaoManager(ConnectionFactory connectionFactory) {
-    this.connFac = connectionFactory;
-  }
+public class DaoManager {
+    protected ConnectionFactory connFac = null;
+    protected Connection conn = null;
 
-  public Connection getConnection() throws SQLException {
-    if (this.conn == null) {
-      this.getConnection().setAutoCommit(false);
-    }
-    return this.conn;
-  }
-
-  public void close() throws SQLException {
-    try {
-      if (this.conn != null && !this.conn.isClosed())
-        this.conn.close();
-    } catch (SQLException e) { throw e; }
-  }
-
-  public Object transaction(DaoCommand command) {
-    try {
-      this.conn.setAutoCommit(false);
-      Object returnValue = command.execute(this);
-      this.conn.commit();
-      return returnValue;
-    } catch (Exception e) {
-      try {
-        this.conn.rollback();
-      } catch (SQLException ex) {
-        ex.printStackTrace();
-      }
-    } finally {
-      try {
-        this.conn.setAutoCommit(true);
-      } catch (SQLException e) {
-        e.printStackTrace();
-      }
-    }
-    return null;
-  }
-
-  public Object executeAndClose(DaoCommand command) {
-    try {
-      return command.execute(this);
-    } finally {
-      try {
-        this.getConnection().close();
-      } catch (SQLException e) {
-        e.printStackTrace();
-      }
+    public DaoManager(ConnectionFactory connectionFactory) {
+        this.connFac = connectionFactory;
     }
 
-    public Object transactionAndClose(final DaoCommand command) {
-      return executeAndClose(new DaoCommand() {
-        @Override
-        public Object execute(DaoManager daoManager) {
-          return daoManager.transaction(command);
+    public Connection getConnection() throws SQLException {
+        if (this.conn == null) {
+            this.getConnection().setAutoCommit(false);
         }
-      });
+        return this.conn;
     }
 
-    public Dao<Customer> getCustomerDao() throws SQLException {
-      return new CustomerDaoImpl(this.getConnection());
+    public void close() throws SQLException {
+        try {
+            if (this.conn != null && !this.conn.isClosed())
+                this.conn.close();
+        } catch (SQLException e) {
+            throw e;
+        }
     }
 
-    public Dao<CreditCard> getCreditCardDao() throws SQLException {
-      return new CreditCardDaoImpl(this.getConnection());
+    public Object transaction(DaoCommand command) {
+        try {
+            this.conn.setAutoCommit(false);
+            Object returnValue = command.execute(this);
+            this.conn.commit();
+            return returnValue;
+        } catch (Exception e) {
+            try {
+                this.conn.rollback();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        } finally {
+            try {
+                this.conn.setAutoCommit(true);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 
-    public Dao<Room> getRoomDao() throws SQLException {
-      return new RoomDaoImpl(this.getConnection());
-    }
+    public Object executeAndClose(DaoCommand command) {
+        try {
+            return command.execute(this);
+        } finally {
+            try {
+                this.getConnection().close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
-    public Dao<Reservation> getReservationDao() throws SQLException {
-      return new ReservationDaoImpl(this.getConnection());
+        public Object transactionAndClose (final DaoCommand command){
+            return executeAndClose(new DaoCommand() {
+                @Override
+                public Object execute(DaoManager daoManager) {
+                    return daoManager.transaction(command);
+                }
+            });
+        }
+
+        public Dao<Customer> getCustomerDao () throws SQLException {
+            return new CustomerDaoImpl(this.getConnection());
+        }
+
+        public Dao<CreditCard> getCreditCardDao () throws SQLException {
+            return new CreditCardDaoImpl(this.getConnection());
+        }
+
+        public Dao<Room> getRoomDao () throws SQLException {
+            return new RoomDaoImpl(this.getConnection());
+        }
+
+        public Dao<Reservation> getReservationDao () throws SQLException {
+            return new ReservationDaoImpl(this.getConnection());
+        }
     }
-}
