b947c63102d245ac660476f688409191c793abd9
nabishop <35745821+nabishop@users.noreply.github.com>
2019-12-04 08:27:25 -0800

    connection factory

---
+++ IntelliJ Files/group01/src/main/java/dao/ConnectionFactory.java
@@ -0,0 +1,31 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    private String driver;
+    private String url;
+    private String user;
+    private String pass;
+
+    public ConnectionFactory(String driver, String url, String user, String pass) {
+        this.driver = driver;
+        this.url = url;
+        this.user = user;
+        this.pass = pass;
+    }
+
+    public Connection getConnection() {
+        try {
+            Class.forName(driver);
+            return DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the db", e);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
