1550ba323f1c66d53d8f90c1a1d750c51638dd80
nabishop <35745821+nabishop@users.noreply.github.com>
2019-12-04 08:59:18 -0800

    Update CreditCardDaoImpl.java

--- IntelliJ Files/group01/src/main/java/dao/CreditCardDaoImpl.java
+++ IntelliJ Files/group01/src/main/java/dao/CreditCardDaoImpl.java
@@ -1,5 +1,6 @@
 package dao;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import entity.CreditCard;
 
 import java.sql.*;
@@ -8,143 +9,133 @@ import java.util.Set;
 
 public class CreditCardDaoImpl implements Dao<CreditCard> {
 
-  private Connection conn;
+    private Connection conn;
 
-  public CreditCardDaoImpl(Connection conn) {
-    this.conn = conn;
-  }
-
-  @Override
-  public CreditCard getById(int id) {
-    CreditCard creditCard = null;
-    PreparedStatement preparedStatement = null;
-    ResultSet resultSet = null;
-    try {
-      preparedStatement = this.conn.prepareStatement("SELECT * FROM CreditCard WHERE CardNum = ?");
-      preparedStatement.setInt(1, id);
-      resultSet = preparedStatement.executeQuery();
-      Set<CreditCard> creditCards = unpackResultSet(resultSet);
-      creditCard = (CreditCard) creditCards.toArray()[0];
-    } catch (SQLException e) {
-      e.printStackTrace();
+    public CreditCardDaoImpl(Connection conn) {
+        this.conn = conn;
     }
-    return creditCard;
-  }
 
-  @Override
-  public Set<CreditCard> getAll() {
-    Set<CreditCard> creditCards = null;
-    PreparedStatement preparedStatement = null;
-    ResultSet resultSet = null;
-    try {
-      preparedStatement = this.conn.prepareStatement("SELECT * FROM CreditCard");
-      resultSet = preparedStatement.executeQuery();
-      creditCards = unpackResultSet(resultSet);
-    } catch (SQLException e) {
-      e.printStackTrace();
+    @Override
+    public CreditCard getById(int id) {
+        CreditCard creditCard = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            preparedStatement = this.conn.prepareStatement("SELECT * FROM CreditCard WHERE CardNum = ?");
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            Set<CreditCard> creditCards = unpackResultSet(resultSet);
+            creditCard = (CreditCard) creditCards.toArray()[0];
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return creditCard;
     }
-    return creditCards;
-  }
 
-  @Override
-  public Integer insert(CreditCard obj) {
-    Integer cardNumber = null;
-    PreparedStatement preparedStatement = null;
-    ResultSet resultSet = null;
-    try {
-      preparedStatement = this.conn.prepareStatement(
-        "INSERT INTO CreditCard (CardNum, Balance, CardLimit, OwnerID) VALUES (?, ?, ?, ?)");
-      preparedStatement.setInt(1, obj.getCardNum());
-      preparedStatement.setFloat(2, obj.getCardLimit());
-      preparedStatement.setFloat(3, obj.getBalance());
-      preparedStatement.setInt(4, obj.getOwnerId());
-      int numRows = preparedStatement.executeUpdate();
-      if (numRows == 1) {
-        // get generated id
-        resultSet = preparedStatement.getGeneratedKeys();
-        if(resultSet.next())
-          cardNumber = resultSet.getInt(1);
-
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    } finally{
-      try {
-        if (resultSet != null)
-          resultSet.close();
-      } catch (SQLException e) {
-        e.printStackTrace();
-      }
-
-      try {
-        if (preparedStatement != null)
-          preparedStatement.close();
-      } catch (SQLException e) {
-        e.printStackTrace();
-      }
+    @Override
+    public Set<CreditCard> getAll() {
+        Set<CreditCard> creditCards = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            preparedStatement = this.conn.prepareStatement("SELECT * FROM CreditCard");
+            resultSet = preparedStatement.executeQuery();
+            creditCards = unpackResultSet(resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return creditCards;
     }
-    return cardNumber;
-  }
 
-  @Override
-  public Integer update(CreditCard obj) {
-    Integer numRows = 0;
-    PreparedStatement preparedStatement = null;
-    try {
-      preparedStatement = this.conn.prepareStatement(
-        "UPDATE CreditCard SET Balance = ?, CardLimit = ?, OwnerID = ? WHERE CardNum = ?",
-        Statement.RETURN_GENERATED_KEYS);
-      preparedStatement.setFloat(1, obj.getBalance());
-      preparedStatement.setFloat(2, obj.getCardLimit());
-      preparedStatement.setInt(3, obj.getOwnerId());
-      preparedStatement.setInt(4, obj.getCardNum());
-      numRows = preparedStatement.executeUpdate();
-    } catch (SQLException e) {
-      e.printStackTrace();
-    } finally{
-      try {
-        if (preparedStatement != null)
-          preparedStatement.close();
-      } catch (SQLException e) {
-        e.printStackTrace();
-      }
+    @Override
+    public Boolean insert(CreditCard obj) {
+        Integer cardNumber = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = this.conn.prepareStatement(
+                    "INSERT INTO CreditCard (CardNum, Balance, CardLimit, OwnerID) VALUES (?, ?, ?, ?)");
+            preparedStatement.setInt(1, obj.getCardNum());
+            preparedStatement.setFloat(2, obj.getCardLimit());
+            preparedStatement.setFloat(3, obj.getBalance());
+            preparedStatement.setInt(4, obj.getOwnerId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return true;
     }
-    return numRows;
-  }
 
-  @Override
-  public Integer delete(CreditCard obj) {
-    Integer numRows = 0;
-    PreparedStatement preparedStatement = null;
-    try {
-      preparedStatement = this.conn.prepareStatement(
-        "DELETE FROM CreditCard WHERE CardNum = ?");
-      preparedStatement.setInt(1, obj.getCardNum());
-      numRows = preparedStatement.executeUpdate();
-    } catch (SQLException e) {
-      e.printStackTrace();
-    } finally{
-      try {
-        if (preparedStatement != null)
-          preparedStatement.close();
-      } catch (SQLException e) {
-        e.printStackTrace();
-      }
+    @Override
+    public Boolean update(CreditCard obj) {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = this.conn.prepareStatement(
+                    "UPDATE CreditCard SET Balance = ?, CardLimit = ?, OwnerID = ? WHERE CardNum = ?",
+                    Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setFloat(1, obj.getBalance());
+            preparedStatement.setFloat(2, obj.getCardLimit());
+            preparedStatement.setInt(3, obj.getOwnerId());
+            preparedStatement.setInt(4, obj.getCardNum());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
     }
-    return numRows;
-  }
 
-  private Set<CreditCard> unpackResultSet(ResultSet rs) throws SQLException {
-    Set<CreditCard> creditCards = new HashSet<CreditCard>();
-
-    while(rs.next()) {
-      CreditCard creditCard = new CreditCard(
-        rs.getInt("CardNum"),
-        rs.getFloat("Balance"),
-        rs.getFloat("CardLimit"),
-        rs.getInt("OwnerID"));
-      creditCards.add(creditCard);
+    @Override
+    public Boolean delete(CreditCard obj) {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = this.conn.prepareStatement(
+                    "DELETE FROM CreditCard WHERE CardNum = ?");
+            preparedStatement.setInt(1, obj.getCardNum());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return true;
     }
-    return creditCards;
-  }
+
+    private Set<CreditCard> unpackResultSet(ResultSet rs) throws SQLException {
+        Set<CreditCard> creditCards = new HashSet<CreditCard>();
+
+        while (rs.next()) {
+            CreditCard creditCard = new CreditCard(
+                    rs.getInt("CardNum"),
+                    rs.getFloat("Balance"),
+                    rs.getFloat("CardLimit"),
+                    rs.getInt("OwnerID"));
+            creditCards.add(creditCard);
+        }
+        return creditCards;
+    }
 }
