8b22854d3c4d5f62874bb8bc51bbf9f7839ce679
nabishop <35745821+nabishop@users.noreply.github.com>
2019-12-04 08:38:53 -0800

    main and credentials

---
+++ IntelliJ Files/group01/src/main/java/Main.java
@@ -0,0 +1,45 @@
+import dao.Dao;
+import dao.DaoManager;
+import dao.ConnectionFactory;
+import entity.Customer;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Set;
+
+public class Main {
+    public static void main(String args[]) {
+        String credFile = "./credentials.xml";
+        Properties properties = new Properties();
+        FileInputStream fis = null;
+
+        try {
+            fis = new FileInputStream(credFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            properties.loadFromXML(fis);
+            DaoManager daoManager = new DaoManager(new ConnectionFactory(
+                    properties.getProperty("driver"),
+                    properties.getProperty("url"),
+                    properties.getProperty("user"),
+                    properties.getProperty("pass")
+            ));
+            Dao<Customer> customerDao = daoManager.getCustomerDao();
+            Set<Customer> customers = customerDao.getAll();
+            for (Customer customer : customers) {
+                System.out.println(customer);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
