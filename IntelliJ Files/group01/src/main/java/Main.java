e35c08e37f9a6c22a19ee0020be77eb8cdc47d07
moconnell1227 <moconn07@calpoly.edu>
2019-12-10 14:35:30 -0800

    Updated main function. Start of cmd interface

--- IntelliJ Files/group01/src/main/java/Main.java
+++ IntelliJ Files/group01/src/main/java/Main.java
@@ -1,6 +1,4 @@
-import dao.Dao;
-import dao.DaoManager;
-import dao.ConnectionFactory;
+import dao.*;
 import entity.CreditCard;
 import entity.Customer;
 import entity.Reservation;
@@ -12,6 +10,9 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.util.Set;
+
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
@@ -34,15 +35,80 @@ public class Main {
                     properties.getProperty("user"),
                     properties.getProperty("pass")
             ));
-            Dao<CreditCard> reservationDao = daoManager.getCreditCardDao();
-            Set<CreditCard> reservations = reservationDao.getAll();
-            for (CreditCard reservation : reservations) {
-                System.out.println(reservation);
+            Dao<CreditCard> dao_for_card = daoManager.getCreditCardDao();
+            CreditCardDaoImpl ccDao = (CreditCardDaoImpl) dao_for_card;
+            Set<CreditCard> creditcards = ccDao.getAll();
+            for (CreditCard card : creditcards) {
+                System.out.println(card);
             }
+
+            Dao<Customer> dao_for_cust = daoManager.getCustomerDao();
+            CustomerDaoImpl custDao = (CustomerDaoImpl) dao_for_cust;
+            Set<Customer> customers = custDao.getAll();
+            for (Customer cust : customers) {
+                System.out.println(cust);
+            }
+
+            Dao<Reservation> dao_for_res = daoManager.getReservationDao();
+            ReservationDaoImpl resDao = (ReservationDaoImpl) dao_for_res;
+            Set<Reservation> reservations = resDao.getAll();
+            for (Reservation res : reservations) {
+                System.out.println(res);
+            }
+
+            Dao<Room> dao_for_room = daoManager.getRoomDao();
+            RoomDaoImpl roomDao = (RoomDaoImpl) dao_for_room;
+            Set<Room> rooms = roomDao.getAll();
+            for (Room r : rooms) {
+                System.out.println(r);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        // MAIN LOOP OF SYSTEM PROGRAM STARTS HERE
+        main_loop();
+    }
+
+    private static void print_options() {
+        System.out.println("1. Make a reservation\n2. Cancel a booking\n3. Get Room information\n");
+    }
+
+    private static void main_loop() {
+        Scanner sc = new Scanner(System.in);
+        boolean run = false;
+        int command;
+        int counter = 0;
+
+        while (run) {
+            System.out.println("Welcome to the 365-Reservation System! Please choose an option:");
+            print_options();
+            command = sc.nextInt();
+            switch(command) {
+                case 1: // call make reservation submenu
+                    System.out.println("You're trying to make a reservation!");
+                    counter=0;
+                    continue;
+                case 2: // call cancel reservation submenu
+                    System.out.println("You're trying to cancel a reservation!");
+                    counter=0;
+                    continue;
+                case 3: // call get room info submenu
+                    System.out.println("You're trying to get some room info!");
+                    counter=0;
+                    continue;
+                default: System.out.print("Unsupported command. Exiting...");
+                    run=false;
+            }
+            if (++counter > 100) {
+                run = false;
+            }
+
+        }
+
+        return;
     }
 }
